f5ba18d2129c548ad218451465d5dca0c72761ea
wayytoogood <huseyinsuna1903@gmail.com>
2021-10-25 18:33:26 +0300

    cart logic added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import { useState } from 'react'
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+import CartProvider from './store/CartProvider'
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false)
+
+  const showCart = () => {
+    setIsCartOpen(true)
+  }
+
+  const closeCart = () => {
+    setIsCartOpen(false)
+  }
+
   return (
     <CartProvider>
-      <Header />
+      {isCartOpen && <div>Cart..</div>}
+      <Header onShowCart={showCart} />
       <main>
         <Meals />
       </main>
     </CartProvider>
-  );
+  )
 }
 
-export default App;
+export default App
